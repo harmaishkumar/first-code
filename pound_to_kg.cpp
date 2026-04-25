de683d1c677e5d247617c9eda2c3ae9f6ee158d2
harmaishkumar <harmaishkumar2rk@gmail.com>
2026-04-25 07:17:39 -0700

    Create pound_to_kg.cpp

---
+++ pound_to_kg.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+	float pound, kg;
+	cout<<"Enter the mass in Pound : ";
+	cin>>pound;
+	kg = pound * 0.454;
+	cout<<"Mass in Kg : ";
+	cout<<kg;	
+}
